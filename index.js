2adb4a2a8a80478a74a76a7f04633bfc447f321d
Kurtis Houser <houserkurtis@gmail.com>
2019-06-07 09:38:37 -0700

    Move xml parsing to separate function

--- index.js
+++ index.js
@@ -10,8 +10,32 @@ let lastGeoJsonCfr = 0;
 async function fetchXmlDocument(xmlUrl) {
   console.log('Fetching CFR XML document from govinfo.gov (large file!)...');
   const response = await fetch(xmlUrl);
-  
+
   return response.text();
+}
+
+function parseXmlDocument(xml) {
+  return new Promise((resolve, reject) => {
+    parseString(xml, (err, result) => {
+      if (err !== null) {
+        reject(err);
+      } else {
+        const { SECTION } = result.CFRGRANULE.SUBPART[0];
+  
+        for (i = 1; i < SECTION.length; i++) { // i = 1 -> skips 9.21 General
+          const cfrIndex = SECTION[i].SECTNO[0].substring(2); // remove '§ ' from beginning
+    
+          xmlCfrData[cfrIndex] = {
+            name: SECTION[i].SUBJECT[0].substring(0, SECTION[i].SUBJECT[0].length - 1), // remove '.' from end
+            cfr_revision_history: SECTION[i].CITA
+              ? SECTION[i].CITA[0]
+              : SECTION[i].SECAUTH[0], // used by 9.126 Santa Clara Valley
+          };
+        }
+        resolve();
+      }
+    });
+  });
 }
 
 function readDirectory(directoryPath) {
@@ -48,22 +72,9 @@ async function readLocalTestFile(filename) { // for local testing, async require
   return fs.readFileSync(`./CFR-${year}-title27-vol1-part9-subpartC.xml`); 
 }
 
-fetchXmlDocument(cfrAppellationsUrl)
 // readLocalTestFile() // for local testing
-  .then(xml => parseString(xml, (err, result) => {
-    const { SECTION } = result.CFRGRANULE.SUBPART[0];
-
-    for (i = 1; i < SECTION.length; i++) { // i = 1 -> skips 9.21 General
-      const cfrIndex = SECTION[i].SECTNO[0].substring(2); // remove '§ ' from beginning
-
-      xmlCfrData[cfrIndex] = {
-        name: SECTION[i].SUBJECT[0].substring(0, SECTION[i].SUBJECT[0].length - 1), // remove '.' from end
-        cfr_revision_history: SECTION[i].CITA
-          ? SECTION[i].CITA[0]
-          : SECTION[i].SECAUTH[0], // used by 9.126 Santa Clara Valley
-      };
-    }
-  }))
+fetchXmlDocument(cfrAppellationsUrl)
+  .then((xml) => parseXmlDocument(xml))
   .then(() => {
     checkForUpdatedCfrs(readDirectory('./avas'));
     checkForUpdatedCfrs(readDirectory('./tbd'));
