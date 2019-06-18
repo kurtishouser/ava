4e9503e3a16db8f610a0dc659cc30b1a73d41b95
Kurtis Houser <houserkurtis@gmail.com>
2019-06-18 12:28:00 -0700

    Replace parseEcfrXml(), parseCfrXml() with parseXml(), extractEcfrAvas(), extractCfrAvas()

--- index.js
+++ index.js
@@ -20,65 +20,62 @@ async function fetchXmlDocument(xmlUrl) {
     : Promise.reject(`Retrieval failed with HTTP status code ${response.status}.`);
 }
 
-function parseCfrXml(xml) {
+function parseXml(xml) {
   return new Promise((resolve, reject) => {
     parser.parseString(xml, (err, result) => {
       if (err !== null) {
         reject('Error parsing XML document.');
       } else {
-        try {
-          // slice(1) removes CFR 9.21 General
-          const avas = result.CFRGRANULE.SUBPART[0].SECTION.slice(1);
-    
-          for (i = 0; i < avas.length; i++) {
-            const cfrIndex = avas[i].SECTNO[0].substring(2); // remove '§ ' from beginning
-
-            xmlCfrData[cfrIndex] = {
-              name: avas[i].SUBJECT[0].substring(0, avas[i].SUBJECT[0].length - 1), // remove '.' from end
-              cfr_revision_history: avas[i].CITA
-                ? avas[i].CITA[0]
-                : avas[i].SECAUTH[0], // used by 9.126 Santa Clara Valley
-            };
-          }
-          resolve();
-        }
-        catch(e) {
-          reject('Unable to parse XML document, unsupported structure.');
-        }
+        resolve(result);
       }
     });
   });
 }
 
-function parseEcfrXml(xml) {
-  return new Promise((resolve, reject) => {
-    parser.parseString(xml, (err, result) => {
-      if (err !== null) {
-        reject('Error parsing XML document.');
-      } else {
-        try {
-          // slice(1) removes CFR 9.21 General
-          const avas = result.DLPSTEXTCLASS.TEXT[0].BODY[0].ECFRBRWS[0].DIV1[0].DIV3[0].DIV4[0].DIV5[6].DIV6[2].DIV8.slice(1);
-  
-          for (i = 0; i < avas.length; i++) {
-            const cfrIndex = avas[i].attr.N.substring(2); // remove '§ ' from beginning
-            const head = avas[i].HEAD[0].split('   ');
-            const name = head[1].substring(0, head[1].length - 1); // remove trailing '.'
-            const cfr_revision_history = avas[i].CITA
-              ? avas[i].CITA[0].text
-              : avas[i].SECAUTH[0].text; // used by 9.126 Santa Clara Valley
-              
-            xmlCfrData[cfrIndex] = { name, cfr_revision_history };
-          }
-          resolve();
-        }
-        catch(e) {
-          reject('Unable to parse XML document, unsupported structure.');
-        }
-      }
-    });
-  });
+function extractEcfrAvas(eCfr) {
+  try {
+    // slice(1) removes CFR 9.21 General
+    const avas = eCfr.DLPSTEXTCLASS.TEXT[0].BODY[0].ECFRBRWS[0].DIV1[0].DIV3[0].DIV4[0].DIV5[6].DIV6[2].DIV8.slice(1);
+
+    for (i = 0; i < avas.length; i++) {
+      const cfrIndex = avas[i].attr.N.substring(2); // remove '§ ' from beginning
+      const head = avas[i].HEAD[0].split('   ');
+      const name = head[1].substring(0, head[1].length - 1); // remove trailing '.'
+      const cfr_revision_history = avas[i].CITA
+        ? avas[i].CITA[0].text
+        : avas[i].SECAUTH[0].text; // used by 9.126 Santa Clara Valley
+
+      xmlCfrData[cfrIndex] = { name, cfr_revision_history };
+    }
+    return xmlCfrData;
+  }
+  catch(e) {
+    return ('Unable to parse XML document, unsupported structure.');
+  }
 }
+
+function extractCfrAvas(cfr) {
+  try {
+    // slice(1) removes CFR 9.21 General
+    const avas = cfr.CFRGRANULE.SUBPART[0].SECTION.slice(1);
+
+    for (i = 0; i < avas.length; i++) {
+      const cfrIndex = avas[i].SECTNO[0].substring(2); // remove '§ ' from beginning
+
+      xmlCfrData[cfrIndex] = {
+        name: avas[i].SUBJECT[0].substring(0, avas[i].SUBJECT[0].length - 1), // remove '.' from end
+        cfr_revision_history: avas[i].CITA
+          ? avas[i].CITA[0]
+          : avas[i].SECAUTH[0], // used by 9.126 Santa Clara Valley
+      };
+    }
+    return xmlCfrData;
+  }
+  catch(e) {
+    return ('Unable to parse XML document, unsupported structure.');
+  }
+}
+
 
 function readDirectory(directoryPath) {
   const filenames = fs.readdirSync(directoryPath);
@@ -129,19 +126,21 @@ if (!year) {
 
   // readLocalTestFile('./ECFR-title27.xml') // for local testing
   fetchXmlDocument(xmlUrl)
-  .then((xml) => parseEcfrXml(xml))
-  .then(() => {
-    checkForUpdatedCfrs(readDirectory('./avas'));
-    checkForUpdatedCfrs(readDirectory('./tbd'));
-    checkForNewCfrs();
-  })
-  .catch(e => console.log(e));
+    .then((xml) => parseXml(xml))
+    .then((eCfr) => extractEcfrAvas(eCfr))
+    .then(() => {
+      checkForUpdatedCfrs(readDirectory('./avas'));
+      checkForUpdatedCfrs(readDirectory('./tbd'));
+      checkForNewCfrs();
+    })
+    .catch(e => console.log(e));
 } else if (year.length === 4 && Number.isInteger(Number(year))) {
   const xmlUrl = `https://www.govinfo.gov/content/pkg/CFR-${year}-title27-vol1/xml/CFR-${year}-title27-vol1-part9-subpartC.xml`;
 
   // readLocalTestFile(`./CFR-${year}-title27-vol1-part9-subpartC.xml`) // for local testing
   fetchXmlDocument(xmlUrl)
-    .then((xml) => parseCfrXml(xml))
+    .then((xml) => parseXml(xml))
+    .then((cfr) => extractCfrAvas(cfr))
     .then(() => {
       checkForUpdatedCfrs(readDirectory('./avas'));
       checkForUpdatedCfrs(readDirectory('./tbd'));
